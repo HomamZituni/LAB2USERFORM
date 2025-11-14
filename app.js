1114bf3b7dcd546964334fde0ea7ee6d77e1709b
HomamZituni <homam93@msn.com>
2025-11-13 20:21:33 -0500

    added input and form validation

--- app.js
+++ app.js
@@ -18,7 +18,72 @@ usernameInput.value = savedUsername;
 });
 
 
-// Task 3.1: Input event listeners 
+// Task Three: Input event listeners 
 
+// username input validation 
+usernameInput.addEventListener("input", () => {
+if (usernameInput.validity.valueMissing) {
+usernameError.textContent = "Username is required";
+} else if (usernameInput.validity.tooShort) {
+usernameError.textContent = "Username must be at least 3 characters";
+} else {
+usernameError.textContent = ""; }
+});
 
+//email input validation
+emailInput.addEventListener("input", () => {
+if (emailInput.validity.valueMissing) {
+emailError.textContent = "Email is required";
+} else if (usernameInput.validity.typeMismatch) {
+emailError.textContent = "please enter valid email";
+} else {
+usernameError.textContent = ""; }
+});
 
+// password input validation
+
+passwordInput.addEventListener("input", () => {
+if (passwordInput.validity.valueMissing) {
+passwordError.textContent = "Password is required";
+} else if (passwordInput.validity.tooShort) {
+passwordError.textContent = "Password must be at least 8 characters";
+} else if (passwordInput.validity.patternMismatch) {
+passwordError.textContent = "Password must include uppercase, lowercase, and a number";
+} else {
+passwordError.textContent = "";
+}
+});
+
+// password matching input
+confirmPasswordInput.addEventListener("input", () => {
+if (confirmPasswordInput.value !== passwordInput.value) {
+confirmPasswordError.textContent = "Passwords do not match";
+} else {
+confirmPasswordError.textContent = "";
+}
+});
+
+// Task Four: Form registration 
+registrationForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+usernameInput.dispatchEvent(new Event("input"));
+emailInput.dispatchEvent(new Event("input"));
+passwordInput.dispatchEvent(new Event("input"));
+confirmPasswordInput.dispatchEvent(new Event("input"));
+
+  if (usernameError.textContent !== "") {
+    usernameInput.focus();
+  } else if (emailError.textContent !== "") {
+    emailInput.focus();
+  } else if (passwordError.textContent !== "") {
+    passwordInput.focus();
+  } else if (confirmPasswordError.textContent !== "") {
+    confirmPasswordInput.focus();
+  } else {
+    // All fields are valid
+    localStorage.setItem("username", usernameInput.value);
+    alert("Registration successful!");
+    registrationForm.reset();
+  }
+});
